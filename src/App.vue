568a56ce2defba90052699cbe7a739a168f87b7a
n2ptune <n2ptune@outlook.com>
2020-07-18 18:42:59 +0900

    add: element ui plugins for ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
-    <router-view />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
