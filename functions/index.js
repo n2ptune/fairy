3a2e65ea4909d0e30d3589e03c800d9b51edfeb4
n2ptune <n2ptune@outlook.com>
2020-07-22 15:06:09 +0900

    test: emulator for cloud functions in local

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions')
+const express = require('express')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+// Express middleware
+// ...
+
+// Express Routes
+// ...
+
+// All request endpoints start prefix /api
+// /api/create
+// /api/send ....
+exports.api = functions.https.onRequest(app)
