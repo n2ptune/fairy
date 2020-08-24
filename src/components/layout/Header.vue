9e6707e63db609d0ba1d305861f26ce4e4423fbf
n2ptune <n2ptune@outlook.com>
2020-08-24 17:09:34 +0900

    web-style: responsive style for header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,27 @@
 <template>
-  <el-container direction="horizontal">
-    <el-header :height="height" class="header">
-      <div>asdfg</div>
-      <div>asdfg</div>
-    </el-header>
-  </el-container>
+  <el-header class="header">
+    <el-row type="flex" justify="center" align="middle" :gutter="20">
+      <!-- 로고 -->
+      <el-col :xs="22" :md="4" :lg="2">
+        <h1 class="logo-text">Fairy</h1>
+      </el-col>
+      <!-- 메뉴 -->
+      <el-col :md="10" :lg="6" class="hidden-sm-and-down">
+        <el-menu :router="true" mode="horizontal">
+          <el-menu-item :route="{ path: '/' }" index="1">
+            Home
+          </el-menu-item>
+          <el-menu-item :route="{ path: '/about' }" index="2">
+            About
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <!-- Create Fairy Button -->
+      <el-col :span="2" class="hidden-xs-only">3</el-col>
+      <!-- 모바일 메뉴 -->
+      <el-col class="hidden-sm-and-up" :span="2">Menu</el-col>
+    </el-row>
+  </el-header>
 </template>
 
 <script>
@@ -16,8 +33,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.header {
-  display: flex;
-  align-items: center;
+.el-menu {
+  border-bottom: none !important;
+
+  &-item {
+    height: auto !important;
+    font-size: 1.05rem !important;
+    border-bottom: none !important;
+  }
+}
+
+.logo-text {
+  // font-weight: bold;
+  font-size: 1.3rem;
 }
 </style>
