ad5cc49e06acb7a62fc6f8fca97db46ee74cb512
n2ptune <n2ptune@outlook.com>
2020-08-31 15:15:26 +0900

    web-style: footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,13 +2,18 @@
   <el-container>
     <el-footer>
       <el-row type="flex" justify="center" :gutter="20">
-        <el-col :span="6">
-          Title and Subscription
+        <el-col v-bind="column">
+          <ul>
+            <li class="font-logo">
+              fairy
+            </li>
+            <li></li>
+          </ul>
         </el-col>
-        <el-col :span="6">
+        <el-col v-bind="column">
           Some Menus
         </el-col>
-        <el-col :span="6">
+        <el-col v-bind="column">
           Some Menus 2
         </el-col>
       </el-row>
@@ -16,12 +21,52 @@
   </el-container>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    column: {
+      xs: 24,
+      md: 8,
+      lg: 6,
+      xl: 4
+    }
+  })
+}
+</script>
+
 <style lang="scss" scoped>
+@import '@/assets/css/_breakpoints.scss';
+@import '@/assets/css/_variables.scss';
+
+.el-container {
+  border-top: 1px solid #ddd;
+  padding: 3rem 0;
+}
+
 .el-footer {
   width: 100%;
 
   & .el-row {
     flex-wrap: wrap;
+
+    & ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    & .el-col {
+      text-align: center;
+
+      & .font-logo {
+        font-size: 1.7rem;
+        color: $color-dark-header-default;
+      }
+
+      @include mobile {
+        margin-bottom: 1.5rem;
+      }
+    }
   }
 }
 </style>
