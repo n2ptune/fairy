d43be18a7542eb45078ce17d1d01ce98e676a305
n2ptune <n2ptune@outlook.com>
2020-10-21 20:37:01 +0900

    fix(server): missing save

--- functions/lib/firebase/check-origin.js
+++ functions/lib/firebase/check-origin.js
@@ -7,15 +7,11 @@ const { db } = require('./')
 exports.checkOrigin = async function (fairyID) {
   const collection = db.collection('fairies')
 
-  try {
-    const querySnapshot = await collection.where('id', '==', fairyID).get()
+  const querySnapshot = await collection.where('id', '==', fairyID).get()
 
-    if (!querySnapshot.empty) {
-      return querySnapshot.docs[0].data().siteAddr
-    } else {
-      throw new Error('Not Found Fairy')
-    }
-  } catch (error) {
-    return error
+  if (!querySnapshot.empty) {
+    return querySnapshot.docs[0].data().siteAddr
+  } else {
+    throw new Error('Not Found Fairy')
   }
 }
