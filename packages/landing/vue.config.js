aa6e79cd48837b94993b9041f59e3db73edd3d15
n2ptune <n2ptune@outlook.com>
2022-07-12 11:46:37 +0900

    ts-loader

--- packages/landing/vue.config.js
+++ packages/landing/vue.config.js
@@ -2,8 +2,28 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    resolve: {
+      extensions: ['.ts', '.tsx']
+    },
     entry: {
-      app: ['./src/main.ts']
+      app: './src/main.ts'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'ts-loader',
+              options: {
+                appendTsSuffixTo: [/\.vue$/]
+              }
+            }
+          ]
+        }
+      ]
     }
   }
 })
